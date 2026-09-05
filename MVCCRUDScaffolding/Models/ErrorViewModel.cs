8fcf6cdc864981703dffcbe748ad26089dc42127
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-09-05 10:20:06 +0200

    Add project files.

---
+++ MVCCRUDScaffolding/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCRUDScaffolding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
